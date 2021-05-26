dbaaebc684c0fec23fa426a25ec97c565bad968d
Rishabh <rishabh@rhatechnology.com>
2021-05-26 11:22:00 +0530

    add order events

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// custom errors
+// Custom errors
 export * from "./errors/bad-request-error";
 export * from "./errors/custom-error";
 export * from "./errors/database-connection-error";
@@ -6,16 +6,20 @@ export * from "./errors/not-found-error";
 export * from "./errors/request-validation-error";
 export * from "./errors/unauthorized-error";
 
-// middlewares
+// Middlewares
 export * from "./middlewares/current-user";
 export * from "./middlewares/error-handler";
 export * from "./middlewares/require-auth";
 export * from "./middlewares/validate-request";
 
-// events
+// Events
 export * from "./events/base-listener";
 export * from "./events/base-publisher";
 export * from "./events/subjects";
+export * from "./events/types/order-status";
+// ticket
 export * from "./events/ticket-created-event";
 export * from "./events/ticket-updated-event";
-export * from "./events/types/order-status";
+// order
+export * from "./events/order-created-event";
+export * from "./events/order-cancelled-event";
